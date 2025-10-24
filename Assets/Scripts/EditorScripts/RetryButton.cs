8f6f904cb6a6030cd848e98c27fee6ac1ac9b3e2
Tuanaoz <tuanaoz05@gmail.com>
2025-10-24 02:16:37 +0100

    RetryButton time fix

--- Assets/Scripts/EditorScripts/RetryButton.cs
+++ Assets/Scripts/EditorScripts/RetryButton.cs
@@ -5,6 +5,7 @@ public class RetryButton : MonoBehaviour
 {
     public void RetryLevel()
     {
+        Time.timeScale = 1f;
         Scene currentScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(currentScene.name);
     }
